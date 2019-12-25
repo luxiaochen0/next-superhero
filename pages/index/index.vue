6d1647ff4a9d8853597d32e8319b2a96c5e4ed16
luxiaochen <494985971@qq.com>
2019-12-25 22:49:26 +0800

    dev

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,14 +1,14 @@
 <template>
 	<view class="page">
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
-			<swiper-item>
-				<image src="" mode=""></image>
+			<!-- <swiper-item>
+				<image src="../../static/carousel/batmanvssuperman.png" mode="" class="carousel"></image>
 			</swiper-item>
 			<swiper-item>
-				<view class="swiper-item"></view>
-			</swiper-item>
-			<swiper-item>
-				<view class="swiper-item"></view>
+				<image src="../../static/carousel/spiderman.png" mode=""></image>
+			</swiper-item> -->
+			<swiper-item v-for="(carousel,index) in carouselList" :key=index>
+				<image :src="carousel.image" mode="" class="carousel"></image>
 			</swiper-item>
 		</swiper>
 	</view>
@@ -18,11 +18,23 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				carouselList: []
 			}
 		},
 		onLoad() {
-
+			//var _this=this;
+			//请求轮播图数据
+			uni.request({
+			    url: 'https://next-hero.oss-cn-shanghai.aliyuncs.com/json/index-carousel-list.json', 
+			    method:"GET",
+				success: (res) => {//箭头函数不需要_this
+			        console.log(res.data);
+					if(res.data.status==200){
+						var carouselList=res.data.data;
+						this.carouselList=carouselList;
+					}
+			    }
+			});
 		},
 		methods: {
 
@@ -31,5 +43,5 @@
 </script>
 
 <style>
-	
+	@import url("index.css");
 </style>
