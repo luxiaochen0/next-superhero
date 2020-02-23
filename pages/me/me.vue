80bc301e69c9e3f9af5b2da2b3a7012d20ae2e59
luxiaochen <494985971@qq.com>
2020-02-24 00:21:46 +0800

    dev

--- pages/me/me.vue
+++ pages/me/me.vue
@@ -1,6 +1,32 @@
 <template>
-	<view>
-		这是个人页面
+	<view class="page page-fill">
+		<view class="header">
+			<view v-if="userIsLogin">
+				<image src="../../static/icos/108x108.png" class="face"></image>
+			</view>
+			<view v-else>
+				<image src="http://next-superhero-static.luxiaochen.com/group1/M00/00/05/CpoxxFw_-5-AFyVyAABLIH8xBTw233.png" class="face"></image>
+			</view>
+			
+			<view class="info-wapper" v-if="userIsLogin">
+				<view class="nickname">
+					NEXT 学院
+				</view>
+				<view class="nav-info">ID:123abc</view>
+			</view>
+			<view v-else>
+				<navigator url="../registLogin/registLogin">
+					<view class="nickname regist-login">
+						注册/登录
+					</view>
+				</navigator>
+			</view>
+			
+			<view class="set-wapper" v-if="userIsLogin">
+				<image src="../../static/icos/settings.png" class="settings"></image>
+			</view>
+
+		</view>
 	</view>
 </template>
 
@@ -8,7 +34,7 @@
 	export default {
 		data() {
 			return {
-				
+				userIsLogin:false
 			}
 		},
 		methods: {
@@ -18,5 +44,5 @@
 </script>
 
 <style>
-
+@import url("me.css");
 </style>
---
+++ pages/registLogin/registLogin.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="body">
+		<form @submit="formSubmit">
+			<view class="face-wapper">
+				<image src="../../static/icos/default-face.png" class="face"></image>
+			</view>
+			
+			<view class="info-wapper">
+				<label class="words-lbl">账号</label>
+				<input name="username" type="text" value="" class="input" placeholder="请输入用户名" placeholder-class="graywords"/>
+			</view>
+			
+			<view class="info-wapper" style="margin-top: 40upx;">
+				<label class="words-lbl">密码</label>
+				<input name="password" type="text" value="" password="true" class="input" placeholder="请输入密码" placeholder-class="graywords"/>
+			</view>
+			
+			<button type="primary" form-type="submit" style="margin-top: 60upx;width: 90%;">注册/登录</button>
+		</form>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			formSubmit(e){
+				var username=e.detail.value.username;
+				var password=e.detail.value.password;
+				
+				
+				
+			}
+		}
+	}
+</script>
+
+<style>
+@import url("registLogin.css");
+</style>
